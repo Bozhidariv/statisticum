6e7bd67cc411bbe8d680cefc20452ed1f3ccd8ba
Bozhidariv <bojidariv@gmail.com>
2015-07-11 15:36:58 +0300

    add tests

--- statisticum/common/templatetags/common_tags.py
+++ statisticum/common/templatetags/common_tags.py
@@ -14,9 +14,11 @@ register = template.Library()
 def share_twitter(item):
     return "https://twitter.com/intent/tweet?text="+item.title+" - &url="
 
+
 @register.filter("share_facebook")
 def share_facebook(item):
     return "https://www.facebook.com/sharer/sharer.php?u=" 
+
 
 @register.filter("share_linkedin")
 def share_linkedin(item):
@@ -27,9 +29,11 @@ def share_linkedin(item):
 def slugify(text):
     return slughifi(text)
 
+
 @register.filter("title")
 def title(title):
     return re.sub("[\(\[].*?[\)\]]", "", title)
+
 
 @register.filter("game_url")
 def game_url(game):
@@ -40,52 +44,3 @@ def game_url(game):
                 result = result + " " + param
     return slughifi(result)
 
-@register.filter("first_letters")
-def first_letters(text,limit=2):
-    letters = [word[0].upper() for word in text.split()][:limit]
-    return "".join(letters)
-
-@register.filter("pretty_date")
-def pretty_date(time,short=True):
-    
-    if type(time) is not int and type(time)!= datetime:
-        return ""
-    """
-    Get a datetime object or a int() Epoch timestamp and return a
-    pretty string like 'an hour ago', 'Yesterday', '3 months ago',
-    'just now', etc
-    """
-    #print timezone.now().strftime('%X %x %Z')
-    time = time.replace(tzinfo=timezone.utc)
-    #print time.strftime('%X %x %Z')
-    now = timezone.now()
-
-    diff = now - time
-  
-    second_diff = diff.seconds
-    day_diff = diff.days
-    if day_diff < 0:
-        return ''
-
-    if day_diff == 0:
-        if second_diff < 10:
-            return  "now" if short else "just now"
-        if second_diff < 60:
-            return str(second_diff) + "s" if short else " seconds ago"
-        if second_diff < 120:
-            return  "1 m" if short else "a minute ago" 
-        if second_diff < 3600:
-            return str( second_diff // 60 ) + " m" if short else " minutes ago"
-        if second_diff < 7200:
-            return "1 h" if short else " an hour ago"
-        if second_diff < 86400:
-            return str( second_diff // 3600 ) + " h" if short else " hours ago"
-    if day_diff == 1:
-        return "1 d" if short else ugettext("Yesterday")
-    if day_diff < 7:
-        return str(day_diff) + " " + "d" if short else ugettext("days ago")
-    if day_diff < 31:
-        return str(int(day_diff//7)) + " w" if short else " weeks ago"
-    if day_diff < 365:
-        return str(int(day_diff//30)) + " months ago"
-    return str(int(day_diff//365)) + " y" if short else " years ago"
--- statisticum/tests/test_views.py
+++ statisticum/tests/test_views.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
- 
+        
 class AccountViewTest(TestCase):
 
     fixtures = ['users.json']
@@ -13,18 +13,66 @@ class AccountViewTest(TestCase):
         
         login_successful = self.client.login(username='admin',password='admin')
         self.assertTrue(login_successful)
-        
 
-class UserProfileViewTest(TestCase):
-    """Unit test for verify profile views"""
- 
+
+class ViewsRestictionsTest(TestCase):
+
+    def check_restriction(self,url,template):
+        response = self.client.get(url,follow=True)
+        self.assertTrue(200, response.status_code)
+        assert not response.context["user"].is_authenticated()
+        self.assertTemplateNotUsed(response, template)
+
+
+    def test_profile(self):
+        self.check_restriction('/profiles/edit/','profiles/edit.html')
+
+
+    def test_game(self):
+        self.check_restriction('/games/','games/index.html')   
+
+
+    def test_game_wins(self):
+        self.check_restriction('/games/wins/','games/wins.html')
+
+
+    def test_game_losts(self):
+        self.check_restriction('/games/losts/','games/losts.html')
+
+
+    def test_game_draws(self):
+        self.check_restriction('/games/draws/','games/draws.html')
+
+class ViewsRenderingTest(TestCase):
+
     fixtures = ['users.json']
- 
+
     def setUp(self):
         login_successful  = self.client.login(username='admin',password='admin')
         self.assertTrue(login_successful)
- 
-    def test_profile_form_success(self):
-        response = self.client.get('/profiles/edit/')
-        self.assertTrue(200,response.status_code)
+
+    def check_rendering(self,url,template):
+        response = self.client.get('/profiles/edit/',follow=True)
+        self.assertTrue(200, response.status_code)
+        assert  response.context["user"].is_authenticated()
         self.assertTemplateUsed(response, 'profiles/edit.html')
+
+
+    def test_profile(self):
+        self.check_rendering('/profiles/edit/','profiles/edit.html')
+
+
+    def test_game(self):
+        self.check_rendering('/games/','games/index.html')   
+
+
+    def test_game_wins(self):
+        self.check_rendering('/games/wins/','games/wins.html')
+
+
+    def test_game_losts(self):
+        self.check_rendering('/games/losts/','games/losts.html')
+
+
+    def test_game_draws(self):
+        self.check_rendering('/games/draws/','games/draws.html')
